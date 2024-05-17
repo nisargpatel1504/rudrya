0be120562937c0fbe1ae287f990a0b0a8fa941ea
nisargpatel1504 <nisargpatel1504@gmail.com>
2024-05-16 22:30:59 -0600

    made system to consume multiple brokers with on consumer

--- consumer.go
+++ consumer.go
@@ -2,20 +2,25 @@ package rudrya
 
 // Consumer is responsible for receiving messages from a broker.
 type Consumer struct {
-    broker *Broker
+    brokers []*Broker
     topic  string
     ch     chan string
 }
 
 // NewConsumer initializes a new Consumer instance and registers it with the broker.
-func NewConsumer(broker *Broker, topic string, bufferSize int) *Consumer {
+func NewConsumer(brokers []*Broker, topic string, bufferSize int) *Consumer {
 	ch := make(chan string, bufferSize)
-	broker.RegisterConsumer(topic, ch)
-	return &Consumer{
-		broker: broker,
-		topic:  topic,
-		ch:     ch,
+    consumer := &Consumer{
+		brokers: brokers,
+		topic:   topic,
+		ch:      ch,
 	}
+
+	for _, broker := range brokers {
+		broker.RegisterConsumer(topic, ch)
+	}
+
+	return consumer
 }
 
 // Start begins listening for messages on the consumer's channel.
